541b7200688fa0364ec67e6481e28bd34a7ac535
tTdly <tutoudenglongyu@163.com>
2022-02-14 21:56:24 +0800

    完成简单控制台程序

---
+++ minigrep/src/main.rs
@@ -0,0 +1,17 @@
+use minigrep::Config;
+use std::{env, process};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("出现了错误：{}", err);
+        process::exit(1);
+    });
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("错误：{}", e);
+        process::exit(1);
+    }
+}
